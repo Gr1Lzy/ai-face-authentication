3918bfacdf412ba3a7f74ef743c36ca10806e337
Andrii Kolomoiets <kolomoets02@gmail.com>
2024-06-06 17:59:08 +0300

    add userController

--- src/main/java/com/example/aifaceauthentication/controller/UserController.java
+++ src/main/java/com/example/aifaceauthentication/controller/UserController.java
@@ -1,5 +1,30 @@
 package com.example.aifaceauthentication.controller;
 
-public class UserController {
+import com.example.aifaceauthentication.dto.user.UserLoginRequestDto;
+import com.example.aifaceauthentication.dto.user.UserLoginResponseDto;
+import com.example.aifaceauthentication.dto.user.UserRegisterRequestDto;
+import com.example.aifaceauthentication.dto.user.UserResponseDto;
+import com.example.aifaceauthentication.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class UserController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public UserResponseDto register(@Validated @RequestBody UserRegisterRequestDto requestDto) {
+        return authenticationService.register(requestDto);
+    }
+
+    @PostMapping("/login")
+    public UserLoginResponseDto authenticate(@Validated @RequestBody UserLoginRequestDto requestDto) {
+        return authenticationService.login(requestDto);
+    }
 }
